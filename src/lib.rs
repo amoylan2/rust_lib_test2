342523197423c446d8eaa1906d6424bb70994faa
Mengran Lan <lanmengran@qq.com>
2024-05-08 23:25:12 +0800

    add static mut object

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 pub struct Test2 {
 
+}
+
+
+lazy_static! {
+    static ref ARRAY: Mutex<Vec<u8>> = Mutex::new(vec![]);
+}
+
+pub fn do_a_call() {
+    ARRAY.lock().unwrap().push(1);
+}
+
+pub fn get_call_count() -> usize {
+    ARRAY.lock().unwrap().len()
 }
 
 // pub unsafe extern "C" fn tt() -> () {
