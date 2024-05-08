98bb614b87ea4f98aff70c0a71d1430a5aa1a2c3
Mengran Lan <lanmengran@qq.com>
2024-05-08 19:48:46 +0800

    init

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+pub struct Test2 {
+
+}
+
+pub unsafe extern "C" fn tt() -> () {
+    println!("hello from test2")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
